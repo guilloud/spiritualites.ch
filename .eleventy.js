6287401636ff1117f38436c63e4a874edcd40723
nfriedli <nicolas.friedli@gmail.com>
2019-09-30 11:19:22 +0200

    Page d'attente.

---
+++ .eleventy.js
@@ -0,0 +1,53 @@
+module.exports = function (eleventyConfig) {
+
+    const markdownIt = require('markdown-it');
+    const markdownItAnchor = require('markdown-it-anchor');
+    const uslug = require('uslug')
+    const uslugify = s => uslug(s)
+
+    eleventyConfig.setLibrary("md",
+        markdownIt({
+            typographer: true
+        }).use(markdownItAnchor, {
+            permalink: true,
+            permalinkCLass: "direct-link",
+            slugify: uslugify,
+            permalinkSymbol: "#"
+        })
+    );
+
+    eleventyConfig.addLayoutAlias('base', 'layouts/base.liquid');
+
+    const htmlmin = require("html-minifier");
+
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+        if (outputPath.endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                collapseWhitespace: true,
+                decodeEntities: true,
+                minifyCSS: true,
+                minifyJS: true,
+                preserveLineBreaks: true,
+                removeComments: true,
+                sortClassName: true,
+                useShortDoctype: true
+            });
+            return minified;
+        }
+
+        return content;
+    });
+
+    eleventyConfig.addPassthroughCopy("src/favicon.ico");
+    eleventyConfig.addPassthroughCopy("src/robots.txt");
+    eleventyConfig.addPassthroughCopy("src/_headers");
+    eleventyConfig.addPassthroughCopy("src/img");
+
+    return {
+        passthroughFileCopy: true,
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+};
---
+++ src/index.md
@@ -0,0 +1,11 @@
+--- 
+title: Spiritualités protestantes
+date: Last modified
+layout: base
+home: true
+permalink: /
+---
+
+En préparation, un site sur la spiritualité chrétienne, en particulier protestante.
+
+Offres des Églises, propositions de cours et conseils de lecture.
